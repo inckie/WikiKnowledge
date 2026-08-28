0a369742a8fe7a53cbea989bfb396f961ce1896b
Ink <i@am.com>
2026-08-28 07:45:29 -0400

    * run.py fix

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Entry point for WikiKnowledge server."""
 
-from wikiknowledge.api.app import main
+from wikiknowledge.cli import main
 
 if __name__ == "__main__":
     main()
